6ec57e1a0c6f5f8567ee4dd058261dd5a692b030
Enrique Novoa <luisenriquenovoa@gmail.com>
2017-06-18 23:57:12 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+var logins = [
+  {
+    username: "enrique",
+    password: "novoa16"
+  },
+  {
+    username: "christopher",
+    password: "payne12"
+  },
+  {
+    username: "dylan",
+    password: "iamdylan"
+  }
+];
+
+var usernameInput = document.getElementById("username");
+var passwordInput = document.getElementById("password");
+var login = document.getElementById("login");
+
+var div = document.createElement("div");
+div.className = "notification";
+var p = document.createElement("p");
+var loginpage = document.getElementById("login-page");
+
+div.appendChild(p);
+loginpage.appendChild(div);
+
+var valid = false;
+
+login.addEventListener("click", function() {
+
+  for (var x = 0; x < logins.length; x++) {
+    if (usernameInput.value == logins[x].username && passwordInput.value == logins[x].password) {
+      valid = true;
+      break;
+    }
+  }
+
+  if (valid) {
+    console.log("Yes!");
+    p.textContent = "Welcome to Codify Academy, " + logins[x].username + "!";
+    p.className = "green";
+
+  } else {
+    console.log("NO!!!");
+    p.textContent = logins;
+    p.nameClass = "red";
+  };
+
+});
+
+
+//
